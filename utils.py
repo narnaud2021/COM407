06fd9f827f3864eec4d322119b707e299288c605
narnaud2021 <63727763+narnaud2021@users.noreply.github.com>
2020-04-29 15:15:54 -0400

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-#defining a minmax function for use as fuzzy system
-def minmax(minn, midd, maxx):
-    return min(maxx, max(minn, midd))
-
-#convert from binary
-def convert(some_list):
-    mylist = some_list
-    mystring = ''.join(str(i) for i in mylist)
-    return int(str(mystring),2)
